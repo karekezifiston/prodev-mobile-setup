e772a60c12821559216883c69fcfb3345fe10fb8
karekezi fiston <karekezifiston30@gmail.com>
2025-10-31 16:13:45 +0200

    aaaa

---
+++ prodev-mobile-app-0x03/app/_layout.tsx
@@ -0,0 +1,7 @@
+// app/_layout.tsx
+import React from "react";
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return <Stack screenOptions={{ headerShown: false }} />;
+}
