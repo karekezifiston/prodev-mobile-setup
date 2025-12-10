bb2c6baed70972cb7a799c78684e8daed717bf70
karekezi fiston <karekezifiston30@gmail.com>
2025-12-10 13:15:04 +0200

    aaaaa

--- prodev-mobile-app-0x00/app/index.tsx
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -1,93 +1,44 @@
-import {
-  Text,
-  TextInput,
-  View,
-  TouchableOpacity,
-  Image,
-} from "react-native";
-import { styles } from "@/styles";
-import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
-import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.navGroup}>
-          <Ionicons name="arrow-back" size={25} />
-          <Image source={require("@/assets/images/logo.png")} />
-        </View>
-        <Text style={styles.largeText}>Sign in to your</Text>
-        <Text style={styles.largeText}>Account</Text>
-        <Text style={styles.smallText}>
-          Enter your email and password to sign in.
-        </Text>
-
-        <View style={styles.formGroup}>
-          <View>
-            <Text style={styles.placeholderText}>Email</Text>
-            <TextInput
-              keyboardType="email-address"
-              style={styles.inputField}
-            />
-          </View>
-          <View style={{ marginTop: 20 }}>
-            <Text style={styles.placeholderText}>Password</Text>
-            <View style={styles.passwordGroup}>
-              <TextInput style={{ flex: 1 }} secureTextEntry />
-              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
-            </View>
-          </View>
-          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
-        </View>
-
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Sign in</Text>
-        </TouchableOpacity>
-
-        <View style={styles.dividerGroup}>
-          <View style={styles.divider}></View>
-          <Text style={styles.dividerText}>OR</Text>
-          <View style={styles.divider}></View>
-        </View>
-
-        <View style={styles.socialMediaButtonGroup}>
-          <TouchableOpacity style={styles.socialMediaButton}>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 5,
-              }}
-            >
-              <Image source={require("@/assets/images/google.png")} />
-              <Text style={styles.socialMediaButtonText}>
-                Continue with Google
-              </Text>
-            </View>
-          </TouchableOpacity>
-
-          <TouchableOpacity style={styles.socialMediaButton}>
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                gap: 5,
-              }}
-            >
-              <Image source={require("@/assets/images/facebook.png")} />
-              <Text style={styles.socialMediaButtonText}>
-                Continue with Facebook
-              </Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-
-        <View style={styles.subTextGroup}>
-          <Text style={styles.subText}>Don't have an account? </Text>
-          <Text style={styles.subTextJoin}>Join now</Text>
-        </View>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+        <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+        <Text style={styles.smallText}>ALX is awesome</Text>
+      </View>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#90caf9",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+  },
+  largeText: {
+    fontSize: 30,
+    color: "#f44336",
+    marginBottom: 5,
+    fontWeight: "700",
+    fontVariant: ["small-caps"],
+    textAlign: "center",
+  },
+  mediumText: {
+    fontSize: 20,
+    color: "#9c27b0",
+    marginBottom: 10,
+    fontWeight: "500",
+    textAlign: "right",
+  },
+  smallText: {
+    fontSize: 15,
+    color: "#2196f3",
+    fontWeight: "400",
+    textAlign: "center",
+  },
+});
